d8527766728362a026034852b27b62d94c970306
Stephen Searles <stephen.searles@gmail.com>
2012-12-02 15:34:51 -0800

    Extending google object to have a function for adding places through the Place API. must be there because of api key

--- app/scripts/sample.google.js
+++ app/scripts/sample.google.js
@@ -1,2 +1,6 @@
-define(["async!https://maps.googleapis.com/maps/api/js?key=API_KEY&libraries=places,geometry&sensor=true!callback"], function () {
+var googleAPIKey = "YOUR_KEY_HERE"
+define(["async!https://maps.googleapis.com/maps/api/js?key="+googleAPIKey+"&libraries=places,geometry&sensor=true!callback"], function () {
+  google.addPlace = function (data) {
+    return $.post("https://maps.googleapis.com/maps/api/place/add/json?sensor=true&key=" + googleAPIKey, data);
+  }
 });
