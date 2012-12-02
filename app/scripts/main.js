de3e6585824f79c37498945409241e44a27d3547
Stephen Searles <stephen.searles@gmail.com>
2012-12-01 21:52:37 -0800

    Added some pretty (gratuitous) animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,11 +12,17 @@ requirejs.config({
   }
 });
 
-require(['app'], function(ComboMap) {
+require(['app','../components/jquery-color/jquery.color'], function(ComboMap) {
   var map = new ComboMap($('.mapspace').get(0));
+  window.map = map;
   $('.directions form').submit(function () {
 
     var $form = $(this);
+    $form.closest('.directions').animate({
+      "padding-top": "0px",
+      "padding-bottom": "0px",
+      "background-color": "white"
+    });
     var $inputs = $form.find('input');
 
     map.getDirections($inputs.eq(0).val(),$inputs.eq(2).val(),$inputs.eq(1).val());
